0e352578e7f36a6f78e8deb934d44bd663306f72
Elias Funes <eliasfunesriveros@gmail.com>
2020-02-07 08:41:05 -0300

    Enabling cross origin resource sharing (cors)

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
+//crear el servidor
 const app = express();
 
+//conectar a la base de datos
 conectarDB();
 
+//habilitar cors
+app.use(cors());
+
+//puerto de la app
 const PORT = process.env.PORT || 4000;
 
+//Habilitar express.json
 app.use(express.json({extended: true }));
 
 app.use('/api/usuarios', require('./src/routes/usuarios'));
